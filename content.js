55cb55c07aab2b0831630eb61eadedb7f9bf97ff
Babatunde Valentine Onabajo <36359216+BabatundeOnabajo@users.noreply.github.com>
2022-12-05 06:50:28 +0000

    Update content.js

--- content.js
+++ content.js
@@ -1,11 +1,9 @@
-// The content.js file is used to obtain information occurring within the website, and outside of the extension itself.
-
 // The content.js file is used to obtain information occurring within the website, and outside of the extension itself. 
 
 //We listen for the event where a user presses Ctrl + P in the section below:
 
 window.addEventListener('keydown', function(event){
-    if(event.keyCode === 80 && (event.ctrlKey || event.metaKey) && !event.altKey && (!event.shiftKey || window.chrome || window.opera)){
+    if((event.keyCode === 80 || e.key === "p" || e.charCode === 16 || e.charCode === 112) && (event.ctrlKey || event.metaKey) && !event.altKey && (!event.shiftKey || window.chrome || window.opera)){
         if(confirm("Please think before you print. Are you sure you would like to continue?") == true){ //Here we use the confirm() method to ask the end user if they do indeed really need to print this online document out. If they would like to continue, then we permit them to print.
             
             window.print(); // This finally allows the user to print 
@@ -18,5 +16,3 @@ window.addEventListener('keydown', function(event){
         
     }
 });
-
-
